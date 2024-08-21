b157291a3ac3101bed603c80e9498f3b6ec91057
uday871 <uday871@gmail.com>
2024-08-21 23:17:28 +0530

    packagejsonchanged

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = 8000;
 
 app.use(bodyParser.json());
 
