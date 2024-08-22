16bc527c225b7408964f65dffd8c72a2de42500b
uday871 <uday871@gmail.com>
2024-08-22 21:58:38 +0530

    changed

--- mylogin/src/components/Userlist1.js
+++ mylogin/src/components/Userlist1.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Userlist1 = () => {
+  
   const [users, setUsers] = useState([]);
   const [error, setError] = useState(null);
+
 
   useEffect(() => {
     axios.get('https://myl-erjr.onrender.com/user')
